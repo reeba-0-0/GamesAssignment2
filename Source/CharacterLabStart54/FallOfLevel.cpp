7778df88d85c64324bc94a2f3d7c8ce29b31abcc
reeba-0-0 <ariba22022@hotmail.com>
2025-03-25 15:00:50 +0000

    only fall off level trigger on the server

--- Source/CharacterLabStart54/FallOfLevel.cpp
+++ Source/CharacterLabStart54/FallOfLevel.cpp
@@ -37,7 +37,7 @@ void AFallOfLevel::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AAct
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 	bool bFromSweep, const FHitResult& SweepResult)
 {
-    if (OtherActor)
+    if (HasAuthority() && OtherActor)
     {
         UE_LOG(LogTemp, Warning, TEXT("%s fell out of the level!"), *OtherActor->GetName());
 
