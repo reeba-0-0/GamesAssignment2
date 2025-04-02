4b6c8ccc597926dea3f5e8f9a463284fdc112666
reeba-0-0 <ariba22022@hotmail.com>
2025-04-02 17:52:55 +0100

    changed build file

--- Source/CharacterLabStart54.Target.cs
+++ Source/CharacterLabStart54.Target.cs
@@ -5,24 +5,17 @@ using System.Collections.Generic;
 
 public class CharacterLabStart54Target : TargetRules
 {
-    private bool bFasterWithoutUnity;
-
-    public CharacterLabStart54Target(TargetInfo Target, bool bFasterWithoutUnity) : base(Target)
+    public CharacterLabStart54Target(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
         DefaultBuildSettings = BuildSettingsVersion.V5;
         IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
         ExtraModuleNames.Add("CharacterLabStart54");
 
-        // Add the missing macro here
-        ExtraModuleNames.AddRange(new string[] { "MyProject" });
-
         bUsePCHFiles = true;
-        bFasterWithoutUnity = true;
 
         // Add the preprocessor macro
         GlobalDefinitions.Add("TEXTURESHARECORE_DEBUGLOG=1");
-        this.bFasterWithoutUnity = bFasterWithoutUnity;
     }
 }
 
